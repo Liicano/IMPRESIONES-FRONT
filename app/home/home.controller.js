02a35de3465516848fd5956c72878c62c203183c
Liicano <hectorluisgonzalezlarreal@gmail.com>
2018-02-07 16:57:42 -0400

    Agregue los primeros 3 contadores dentro del home

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -6,10 +6,33 @@
     .module('app')
     .controller('HomeController', homeController);
 
-  homeController.$inject = ['$scope', '$rootScope', 'appService'];
+  homeController.$inject = ['$scope', '$rootScope', 'appService', '$http'];
 
-  function homeController($scope, $rootScope, appService) {
-    console.log("HOME CONTROLLER");
+  function homeController($scope, $rootScope, appService, $http) {
+     //OBTENER EQUIPOS
+    $http.get("http://localhost:3000/equipos")
+    .then(function(response) {
+        // console.log(response.data);
+        $scope.equipos = [];
+        angular.copy(response.data, $scope.equipos);
+        $scope.cantidad_equipos = $scope.equipos.length;
+        console.log($scope.cantidad_equipos);
+
+        //CANTIDAD DE EQUIPOS DISPONIBLES
+        $scope.cantidad_disponibles = 0;
+        $scope.cantidad_mantenimiento = 0;
+        $scope.cantidad_fueraservicio = 0;
+        for (var i = 0; i < $scope.equipos.length; i++) {
+          if ($scope.equipos[i].estado_equipo == 'DISPONIBLE') $scope.cantidad_disponibles++; 
+          if ($scope.equipos[i].estado_equipo == 'MANTENIMIENTO') $scope.cantidad_mantenimiento++; 
+          if ($scope.equipos[i].estado_equipo == 'FUERA_SERVICIO') $scope.cantidad_fueraservicio++; 
+        };
+        console.log($scope.cantidad_disponibles);
+
+    });
+
+
+
   }
 
 })();
